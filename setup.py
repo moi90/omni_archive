7a7c45f7c61b905efb73d043893ce7d92f9a760d
Martin Schroeder <mschroeder@geomar.de>
2024-12-15 13:51:58 +0100

    Drop 3.9 support

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        # "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
