6196d9e3e82bda2f4679773e789c2569c53702b4
Martin Schroeder <mschroeder@geomar.de>
2025-08-07 09:59:33 +0200

    Remove _version.py and update imports in __init__.py

--- omni_archive/__init__.py
+++ omni_archive/__init__.py
@@ -5,13 +5,11 @@ This Python module provides a generic archive reader and writer for various arch
 including ZIP, TAR, and regular filesystem directories.
 """
 
+from ._version import __version__  # noqa: F401
+from .dir import DirectoryArchive
 from .generic import Archive, UnknownArchiveError
 from .tar import TarArchive
 from .zip import ZipArchive
-from .dir import DirectoryArchive
 
 __all__ = ["Archive", "UnknownArchiveError", "TarArchive", "ZipArchive", "DirectoryArchive"]
 
-from . import _version
-
-__version__ = _version.get_versions()["version"]
